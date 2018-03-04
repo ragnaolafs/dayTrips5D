9340a0e3554ac517e5535666238b64d726c8984a
hugrun94 <33096400+hugrun94@users.noreply.github.com>
2018-03-04 14:22:29 +0000

    Class created

---
+++ DayTrips/src/Trip.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Trip {
+	private String tripID = "";
+	private ArrayList<String> dates = new ArrayList<String>();
+	private String time ="";
+	private String nameOfTrip ="";
+	private String description ="";
+	private int price = 0;
+	private ArrayList<String> types = new ArrayList<String>();
+	private String duration ="";
+	private int capacity = 0;
+	private boolean soldOut = false;
+	private String location = "";
+	
+	
+	public static void updateCapacity(String tripID, )
+	
+	
+	
+
+}

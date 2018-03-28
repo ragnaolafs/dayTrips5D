1cd31c73a936238900b4aa449bca02437a12b106
drekaflugan <kop1@hi.is>
2018-03-28 21:17:48 +0000

    details

--- DayTrips/DayTrips5D/src/View/Search.java
+++ DayTrips/DayTrips5D/src/View/Search.java
@@ -5,27 +5,33 @@
  */
 package View;
 
-import Model.HostLogin;
 import Model.Trip;
+import java.util.ArrayList;
 
 /**
  *
  * @author karen
  */
-public class PostTrip extends javax.swing.JFrame {
+public class Search extends javax.swing.JFrame {
     
-    private Trip tripObject;
-    private HostLogin host;
+    private ArrayList<String> type;
+    private int priceRange;
+    private int priceHigher;
+    private String location;
+    private String keyword;
+    private String duration;
+    private String[] dates;
 
     /**
-     * Creates new form PostTrip
+     * Creates new form Search
      */
-    public PostTrip() {
+    public Search() {
         initComponents();
     }
     
-    public static void post(Trip trip){
-        //
+    public ArrayList<Trip> search(String string) {
+        ArrayList<Trip> searchResult = new ArrayList();
+        return searchResult;
     }
 
     /**
@@ -70,23 +76,24 @@ public class PostTrip extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PostTrip.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Search.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PostTrip.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Search.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PostTrip.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Search.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PostTrip.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Search.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PostTrip().setVisible(true);
+                new Search().setVisible(true);
             }
         });
     }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }

e118b4d4977b84c96ffe620a018c14511afdda62
ragnaolafs <rao9@hi.is>
2018-03-30 14:37:11 +0000

    added functions in DatabaseConnection

--- DayTrips/DayTrips5D/src/Controller/DatabaseConnection.java
+++ DayTrips/DayTrips5D/src/Controller/DatabaseConnection.java
@@ -14,22 +14,22 @@ import java.sql.*;
 public class DatabaseConnection {
 	
 	
-	Connection connection = null;
-	Statement stmt = null;
-	ResultSet r = null;
-	
-	public DatabaseConnection() {
-		try {
-			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection("jdbc:sqlite:dayTripsDB.db");
-			stmt = connection.createStatement();
-		}
-		catch (Exception e) {
-			
-		}
-	}
-	
-	public static void main(String[] args) {
-		
-	}
+    Connection connection = null;
+    Statement stmt = null;
+    ResultSet r = null;
+
+    public DatabaseConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:dayTripsDB.db");
+            stmt = connection.createStatement();
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

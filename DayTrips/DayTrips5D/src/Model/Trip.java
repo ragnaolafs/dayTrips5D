aa3d5e54254a485b1efd5f95cb3256fa5897d462
hugrun94 <33096400+hugrun94@users.noreply.github.com>
2018-03-31 22:33:04 +0000

    TripsView almost finished, Trip(model) probably finished

--- DayTrips/DayTrips5D/src/Model/Trip.java
+++ DayTrips/DayTrips5D/src/Model/Trip.java
@@ -12,22 +12,109 @@ import java.util.*;
  * @author ragna
  */
 public class Trip {
-	private String tripID = "";
-	private ArrayList<String> dates = new ArrayList<String>();
-	private String time ="";
-	private String nameOfTrip ="";
-	private String description ="";
-	private int price = 0;
-	private ArrayList<String> types = new ArrayList<String>();
-	private String duration ="";
-	private int capacity = 0;
-	private boolean soldOut = false;
-	private String location = "";
+	private int tripID;
+	private ArrayList<String> dates;
+	private String time;
+	private String nameOfTrip;
+	private String description;
+	private int price;
+	private ArrayList<String> types;
+	private String duration;
+	private int capacity;
+	private boolean soldOut;
+	private String location;
 	
-	
-	public static void updateCapacity(String tripID) {
+        
+        public Trip(int ID, ArrayList<String> DATES,String TIME,String NAMEOFTRIP,String DESCR,
+                int PRICE,ArrayList<String> TYPES,String DURATION,int CAPACITY,boolean SOLDOUT,String LOCATION){
+            
+            this.tripID=ID;
+            this.dates=DATES;
+            this.time=TIME;
+            this.nameOfTrip=NAMEOFTRIP;
+            this.description=DESCR;
+            this.price=PRICE;
+            this.types=TYPES;
+            this.duration=DURATION;
+            this.capacity=CAPACITY;
+            this.soldOut=SOLDOUT;
+            this.location=LOCATION;
             
         }
+         /**
+         * GETTERS
+         */
+        
+        public int getTripID(){
+            return tripID;
+            
+        }
+        public ArrayList<String> getDates(){
+            return dates;
+        }
+        public String getTime(){
+            return time;
+        }
+        public String getName(){
+            return nameOfTrip;
+        }
+        public String getDescription(){
+            return description;
+        }  
+        public int getPrice(){
+            return price;
+        }
+	public ArrayList<String> getTypes(){
+            return types;
+        }
+        public String getDuration(){
+            return duration;
+        }
+        public int getCapacity(){
+            return capacity;
+        }
+        public boolean getSoldOut(){
+            return soldOut;
+        }
+        public String getLocation(){
+            return location;
+        }
+        
+        /**
+         * SETTERS
+         */
+        /*
+        public void setDates(){
+            
+        }
+        public void setTime(){
+            
+        }
+        public void setName(){
+            
+        }
+        public void setDescription(){
+            
+        }  
+        public void setPrice(){
+            
+        }
+	public void setTypes(){
+            
+        }
+        public void setDuration(){
+            
+        }
+        public void setCapacity(){
+            
+        }
+        public void setSoldOut(){
+            
+        }
+        public void setLocation(){
+            
+        }*/
+        
         
         public static void main(String[] args) {
             

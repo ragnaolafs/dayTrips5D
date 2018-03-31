f815e86daebd8112ac1d8bd5b85e543a2ad13647
ragnaolafs <rao9@hi.is>
2018-03-31 18:34:47 +0000

    worked on DatabaseConnection

--- DayTrips/DayTrips5D/src/Controller/DatabaseConnection.java
+++ DayTrips/DayTrips5D/src/Controller/DatabaseConnection.java
@@ -5,7 +5,9 @@
  */
 package Controller;
 
+import Model.Trip;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,15 +21,62 @@ public class DatabaseConnection {
 	ResultSet r = null;
 	
 	public DatabaseConnection() {
-		try {
-			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection("jdbc:sqlite:dayTripsDB.db");
-			stmt = connection.createStatement();
-		}
-		catch (Exception e) {
-			
-		}
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:dayTripsDB.db");
+                stmt = connection.createStatement();
+            }
+            catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
 	}
+        
+        
+        public void insert(String insertValues) {
+            try {
+                stmt.executeUpdate(insertValues);
+            }
+            catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        
+        
+        public void delete(String deleteValues) {
+            try {
+                stmt.executeUpdate(deleteValues);
+            }
+            catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        
+        
+        public ResultSet select(String selectQuery) {
+            ResultSet rs = null;
+            try {
+                rs = stmt.executeQuery(selectQuery);
+                
+            }
+            catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+            return rs;
+        }
+        
+        
+        
+        public ArrayList<Trip> getTripList() {
+            ArrayList<Trip> tripList = new ArrayList<Trip>();
+            
+            String q = "SELECT * FROM Trips";
+            
+            ResultSet rs = null;
+            
+            Trip trip;
+            
+            return tripList;
+        }
 	
 	public static void main(String[] args) {
 		

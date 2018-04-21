f13c8ee83885f44d9cae9fca10ab0aecb9fad17d
drekaflugan <kop1@hi.is>
2018-04-21 15:13:23 +0000

    documentation almost finished

--- DayTrips/DayTrips5D/src/Model/Booking.java
+++ DayTrips/DayTrips5D/src/Model/Booking.java
@@ -6,32 +6,40 @@
 package Model;
 
 import Controller.DatabaseController;
-import java.util.ArrayList;
+
 /**
- *
- * @author ragna
+ * This class handles the logic of booking a trip. It handles the booking of 
+ * trips and it updates the capacity of trips when bookings are made.
+ * @author Ragna Ólafsdóttir, rao9@hi.is
+ * @author Hugrún Guðmundsdóttir, hug17@hi.is
+ * @author Karen Ósk Pétursdóttir, kop1@hi.is
  */
 
 public class Booking {
 
-
-
+    /**
+     * This method calls the DatabaseController class and makes it add the
+     * trip that is to be booked into the database.
+     * @param name
+     * @param date
+     * @param email
+     * @param pax
+     * @param tripID
+     * @param phoneNo 
+     */
     public static void bookTrip(String name, String date, String email, int pax, int tripID, String phoneNo) {
         DatabaseController dbctr = new DatabaseController();
         dbctr.insertBooking(name, date, email, pax, tripID, phoneNo);
     }
+    
+    /**
+     * This method calls the DatabaseController class and makes it update 
+     * existing information about the remaining capacity of a newly booked trip.
+     * @param pax
+     * @param tripID 
+     */
     public static void updateTripCapacity(int pax, int tripID) {
         DatabaseController dbctr = new DatabaseController();
         dbctr.updateTripCapacity(tripID, pax);
-
-
     }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-    }
-
-
-
 }
-
--- DayTrips/DayTrips5D/src/Model/HostLogin.java
+++ DayTrips/DayTrips5D/src/Model/HostLogin.java
@@ -8,14 +8,17 @@ package Model;
 import Controller.DatabaseController;
 
 /**
- *
- * @author karen
+ * 
+ * @author Ragna Ólafsdóttir, rao9@hi.is
+ * @author Hugrún Guðmundsdóttir, hug17@hi.is
+ * @author Karen Ósk Pétursdóttir, kop1@hi.is
  */
 public class HostLogin {
     private String username = "";
     private static String password = "";	
     private String hostName = "";
     private static String loggedInUser ="";
+    
     public HostLogin(String fullname, String username, String password){
         this.hostName=fullname;
         this.username=username;
@@ -30,30 +33,37 @@ public class HostLogin {
     public HostLogin() {
       this.username = username;
     }
-
 	
-	public void login(String username, String password) {
-            loggedInUser = username;
-            System.out.println(username);
-            DatabaseController logincontrol = new DatabaseController();
-            logincontrol.login(username,password);
-            
-            
-		
-	}
-        public void createNewUser(String fullname, String username, String password){
-             
-            DatabaseController newuser = new DatabaseController();
-            newuser.createNewUser(fullname,username,password);
-            
-        }
-        public String getUsername(){
-            return this.username;
-        }
-        public String getPassword(){
-            return this.password;
-        }
-        public String getLoggedIn(){
-            return loggedInUser;
-        }
+    public void login(String username, String password) {
+        loggedInUser = username;
+        System.out.println(username);
+        DatabaseController logincontrol = new DatabaseController();
+        logincontrol.login(username,password);
+    }
+    
+    /**
+     * Creates a new host in the system with the typed-in username and password
+     * from the user and inserts the new host into the database.
+     * @param fullname
+     * @param username
+     * @param password 
+     */
+    public void createNewUser(String fullname, String username, String password){
+        DatabaseController newuser = new DatabaseController();
+        newuser.createNewUser(fullname,username,password);
+    }
+    
+    /**
+     * Getters
+     * @return 
+     */
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public String getLoggedIn(){
+        return loggedInUser;
+    }
 }
